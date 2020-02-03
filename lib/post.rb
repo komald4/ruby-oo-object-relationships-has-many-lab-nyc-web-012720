080220e0d4ecf19f3329f400f0265a8b734accdf
Komal Dindiyal <komald.4@gmail.com>
2020-02-03 22:17:51 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,40 @@
 class Post
-  attr_accessor :title, :Author
+
+  attr_accessor :title, :author
+
+  @@all = []
+
   def initialize(title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
-    if @author == nil
-      return nil
+    if self.author.nil?
+      nil
     else
-      @author.name
+      self.author.name
     end
   end
-end
+
+
+
+
+# class Post
+#   attr_accessor :title, :Author
+#   def initialize(title)
+#     @title = title
+#   end
+#
+#   def author_name
+#     if @author == nil
+#       return nil
+#     else
+#       @author.name
+#     end
+#   end
+# end
