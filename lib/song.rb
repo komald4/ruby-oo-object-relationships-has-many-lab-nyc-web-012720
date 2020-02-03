629434841a0b4d1fedf50eba1f59f20eb32c78c2
Komal Dindiyal <komald.4@gmail.com>
2020-02-03 22:19:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,48 @@
 class Song
-  attr_accessor :artist, :name, :genre
+
+  attr_accessor :name, :artist
 
   @@all = []
 
-
   def initialize(name)
     @name = name
-    # @@count += 1
-    save
-  end
-  
-  def save
     @@all << self
   end
 
-  def self.all #class method
+  def self.all
     @@all
   end
-end
+
+  def artist_name
+    if self.artist.nil?
+      nil
+    else
+      self.artist.name
+    end
+  end
+
+end 
+
+
+
+
+# class Song
+#   attr_accessor :artist, :name, :genre
+#
+#   @@all = []
+#
+#
+#   def initialize(name)
+#     @name = name
+#     # @@count += 1
+#     save
+#   end
+#
+#   def save
+#     @@all << self
+#   end
+#
+#   def self.all #class method
+#     @@all
+#   end
+# end
