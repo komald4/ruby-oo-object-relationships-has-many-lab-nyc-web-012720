2f480ff08e687734bf7729994f3f8124f6d36868
Komal Dindiyal <komald.4@gmail.com>
2020-02-03 22:18:10 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -20,21 +20,3 @@ class Post
       self.author.name
     end
   end
-
-
-
-
-# class Post
-#   attr_accessor :title, :Author
-#   def initialize(title)
-#     @title = title
-#   end
-#
-#   def author_name
-#     if @author == nil
-#       return nil
-#     else
-#       @author.name
-#     end
-#   end
-# end
