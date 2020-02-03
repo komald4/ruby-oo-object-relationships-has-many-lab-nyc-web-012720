c9188dc057c979d54770a6092107ae0dd9e55b39
Komal Dindiyal <komald.4@gmail.com>
2020-02-03 22:17:05 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,30 +2,28 @@ class Author
 
   attr_accessor :name, :posts
 
-  @@post_count = 0
-
   def initialize(name)
     @name = name
     @posts = []
   end
 
   def add_post(post)
-    @posts << post
     post.author = self
-    @@post_count += 1
+    @posts << post
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+    add_post(post)
   end
 
   def self.post_count
-    @@post_count
+    Post.all.length
   end
-end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
 
 
 
